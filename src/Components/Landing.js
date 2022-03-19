476aa0a1ca7262185e660bccf0bfd1006e66fffe
Vinit Gupta <vinitman35@gmail.com>
2022-03-19 18:16:09 +0530

    add : add color themes

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -7,12 +7,12 @@ function Landing() {
         <div className='typed'>
             <div className='landing-head'>Typr 2.0</div>
             <div className='landing-subhead'>
-            Your arena to type!
+            Your arena to type
         </div>
         </div>
         <div className='landing-desc'>
             <div className='landing-desc-text'>
-                Join and challenge your friends for a Battle Royal...
+                Join and challenge your friends for a Battle Royale...
             </div>
         </div>
     </div>
